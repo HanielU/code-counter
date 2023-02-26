d30dd4df40d2628f1440dff43052bb384f088a96
Haniel Ubogu <hanieltobi@gmail.com>
2023-02-26 23:03:11 +0100

    added chars count & other stuff

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,19 @@ use std::fs;
 use std::io::{StdoutLock, Write};
 use std::path::Path;
 
-fn count_lines_in_file(file_path: &str) -> Result<usize, Box<dyn Error>> {
+fn count_lines_in_file(
+    file_path: &str,
+    count_chars: bool,
+) -> Result<(usize, usize), Box<dyn Error>> {
     let contents = fs::read_to_string(file_path)?;
-    Ok(contents.lines().count())
+    let line_count = contents.lines().count();
+
+    if count_chars {
+        let char_count = contents.chars().count();
+        Ok((line_count, char_count))
+    } else {
+        Ok((line_count, 0))
+    }
 }
 
 fn count_lines_in_directory(
@@ -14,11 +24,13 @@ fn count_lines_in_directory(
     ignore_folders: [&str; 23],
     total_files: &mut usize,
     total_lines: &mut usize,
+    total_chars: &mut usize,
     bad_files: &mut usize,
     handle: &mut StdoutLock,
     log_all_files: bool,
     log_counted_files: bool,
     log_bad_files: bool,
+    count_chars: bool,
 ) -> Result<(), Box<dyn Error>> {
     let path_str = path.to_str().unwrap();
     if ignore_folders
@@ -38,15 +50,26 @@ fn count_lines_in_directory(
             && ignore_files.iter().all(|file| !file_name.contains(file))
         {
             *total_files += 1;
-            match count_lines_in_file(file_name) {
-                Ok(count) => {
-                    *total_lines += count;
+            match count_lines_in_file(file_name, count_chars) {
+                Ok((line_count, char_count)) => {
+                    *total_lines += line_count;
+                    if count_chars {
+                        *total_chars += char_count;
+                    }
                     if log_all_files || log_counted_files {
-                        write!(
+                        if count_chars {
+                            write!(
                             handle,
-                            "lines of code in {} = {}, total = {}\n",
-                            file_name, count, total_lines
+                            "lines of code in {} = {}, {} chars, total lines = {}\n",
+                            file_name, line_count, char_count, total_lines
                         )?;
+                        } else {
+                            write!(
+                                handle,
+                                "lines of code in {} = {}, total lines = {}\n",
+                                file_name, line_count, total_lines
+                            )?;
+                        }
                     }
                 }
                 Err(_) => {
@@ -67,11 +90,13 @@ fn count_lines_in_directory(
                 ignore_folders,
                 total_files,
                 total_lines,
+                total_chars,
                 bad_files,
                 handle,
                 log_all_files,
                 log_counted_files,
                 log_bad_files,
+                count_chars,
             )?;
         }
     }
@@ -89,12 +114,14 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut total_lines = 0;
     let mut total_files = 0;
+    let mut total_chars = 0;
     let mut bad_files = 0;
     let stdout = std::io::stdout();
     let mut handle = stdout.lock();
     let log_all_files = args.len() > 1 && args[1] == "--log-all";
     let log_counted_files = args.len() > 1 && args[1] == "--log-counted";
     let log_bad_files = args.len() > 1 && args[1] == "--log-bad";
+    let count_characters = args.iter().any(|x| x == "--chars");
 
     count_lines_in_directory(
         project_root,
@@ -102,16 +129,22 @@ fn main() -> Result<(), Box<dyn Error>> {
         ignore_folders,
         &mut total_files,
         &mut total_lines,
+        &mut total_chars,
         &mut bad_files,
         &mut handle,
         log_all_files,
         log_counted_files,
         log_bad_files,
+        count_characters,
     )?;
+
+    if count_characters {
+        write!(handle, "Counted a total of {} characters\n", total_chars)?;
+    }
 
     write!(
         handle,
-        "\nAcross {} files, Counted a total of {} lines, encountered {} BAD FILES\n\nBAD FILES are files that could not be counted, e.g (images, audio, etc..).\n",
+        "Across {} files, Counted a total of {} lines, encountered {} BAD FILES\n\nBAD FILES are files that could not be counted, e.g (images, audio, etc..).\n",
         total_files, total_lines, bad_files
     )?;
 
